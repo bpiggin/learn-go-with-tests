a3819866573eaa8180cdff23a185cc52b87c0c89
Benjamin Piggin <piggin.ben@gmail.com>
2022-04-29 16:16:45 +0100

    Refactor

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Ben")
-	want := "Hello, Ben!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+	t.Run("Says hello to a person", func(t *testing.T) {
+		got := Hello("Ben")
+		want := "Hello, Ben"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Says hello to the world if a person is not provided", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
